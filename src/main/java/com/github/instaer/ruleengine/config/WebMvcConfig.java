4d7dfa5f1d8012a10b44eabf5012c4b3567a7e9a
instaer <33076883+instaer@users.noreply.github.com>
2023-07-27 10:00:03 +0800

    Add cors configuration.

---
+++ src/main/java/com/github/instaer/ruleengine/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.github.instaer.ruleengine.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
