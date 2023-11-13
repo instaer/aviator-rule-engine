9d16e22e562dc9c554a8a903537003f0ae626880
instaer <33076883+instaer@users.noreply.github.com>
2023-11-13 11:52:35 +0800

    Cancel automatic trim when entering text

--- src/main/resources/static/_next/static/chunks/pages/ruleset-info-c5e10126c4343a2c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[739],{1704:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/ruleset-info",function(){return u(1844)}])}},function(n){n.O(0,[300,287,844,774,888,179],function(){return n(n.s=1704)}),_N_E=n.O()}]);
---
+++ src/main/resources/static/_next/static/chunks/pages/ruleset-info-e121e64008c9e60a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[739],{4626:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/ruleset-info",function(){return u(5508)}])}},function(n){n.O(0,[900,803,508,774,888,179],function(){return n(n.s=4626)}),_N_E=n.O()}]);
